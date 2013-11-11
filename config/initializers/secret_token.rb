df6551d37079ee7627f060cde7167d0626b22f34
Meredith Underell <m.underell@gmail.com>
2013-11-10 16:38:40 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HotelSearch::Application.config.secret_token = '67a9bcb81aee7235a837d9a40af5d0be603d89f53f8a0cbb60e18ad7d968748080312e0112f13bd5235a94c872cb8018e4ad7b9883464dee7d76a188481c89c4'
